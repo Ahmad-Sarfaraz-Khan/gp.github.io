222f7d2ef224bdada2632b60e96dd8c080fbbd89
Ahmad-Sarfaraz-Khan <94857063+Ahmad-Sarfaraz-Khan@users.noreply.github.com>
2021-12-26 19:56:21 +0530

    Add files via upload

---
+++ gp.js
@@ -0,0 +1,12 @@
+var a = 3; 
+a=parseInt(a);
+var r = 4; 
+r=parseInt(r);
+var n = 7;
+n=parseInt(n);
+
+var nterm;
+    for (var i = 0; i < n; i++) {
+        nterm = a * Math.pow(r, i);
+        console.log(nterm + " ");
+    }
